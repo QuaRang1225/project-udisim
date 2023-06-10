989ee81d63499ed2a7d76bc798a96ffbae9b650f
QuaRang1225 <dbduddnd1225@gmail.com>
2023-06-10 22:40:29 +0900

    firebase 업데이트 및 db저장 로직 변경

--- UdiSIM/AuthenticationViewModel.swift
+++ UdiSIM/AuthenticationViewModel.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import Firebase
+import FirebaseCore
+import FirebaseFirestoreSwift
 import Combine
 
 class AuthenticationViewModel:ObservableObject{
@@ -42,12 +44,20 @@ class AuthenticationViewModel:ObservableObject{
             
             
             let data = ["email":email,"name":name,"password":password,"uid":name + user.uid]    //회원가입 정보를 데이터에 저장
-            Firestore.firestore().collection("user")    //"user" 데이터베이스에 저장
-                .document(name + user.uid)                     //문서명 유저.uid로 설정
-                .setData(data) { aaa in                   //데이터 저장 시 변경 사항
+            
+            Database.database().reference()
+                .child("user")
+                .childByAutoId()
+                .setValue(name + user.uid){ (error,ref) in
                     self.successAuth.send()          //회원정보 저장 후 프로필 사진 변경 화면으로 넘어가기 위함
                     print("DEBUG : 유저정보가 업로드 되었습니다. 프로필 사진 변경")
                 }
+//            Firestore.firestore().collection("user")    //"user" 데이터베이스에 저장
+//                .document(name + user.uid)                     //문서명 유저.uid로 설정
+//                .setData(data) { aaa in                   //데이터 저장 시 변경 사항
+//                    self.successAuth.send()          //회원정보 저장 후 프로필 사진 변경 화면으로 넘어가기 위함
+//                    print("DEBUG : 유저정보가 업로드 되었습니다. 프로필 사진 변경")
+//                }
         }
         
     }
@@ -79,12 +89,18 @@ class AuthenticationViewModel:ObservableObject{
         guard let uid = tempUserSession?.uid else{ return } //유저정보가 등록되어있지 않으면 반환
         
         ImageUploader.uploadImage(image: image){ profileImageUrl in
-            Firestore.firestore().collection("user")    //user에 정보 저장
-                .document(uid)  //document는 uid값으로 지정
-                .updateData(["profileImageUrl":profileImageUrl]){ _ in
-                    self.userSession = self.tempUserSession //userSession - 어플 전체에서 사용될 유저 정보, tempUserSession - 클래스내에서 메서드 실행 시에 사용될 유저 정보
-                    self.fetchUser()    //회원가입 후 모델에 정보를 업데이트
+            Database.database().reference()
+                .child("user")
+                .childByAutoId()
+                .setValue(["profileImageUrl":profileImageUrl]){
+                    
                 }
+//            Firestore.firestore().collection("user")    //user에 정보 저장
+//                .document(uid)  //document는 uid값으로 지정
+//                .updateData(["profileImageUrl":profileImageUrl]){ _ in
+//                    self.userSession = self.tempUserSession //userSession - 어플 전체에서 사용될 유저 정보, tempUserSession - 클래스내에서 메서드 실행 시에 사용될 유저 정보
+//                    self.fetchUser()    //회원가입 후 모델에 정보를 업데이트
+//                }
         }
     }
     func logOut(){
@@ -103,27 +119,27 @@ class AuthenticationViewModel:ObservableObject{
     
     func fetchRecentChat(){
         guard let uid = Auth.auth().currentUser?.uid else{ return }
-        Firestore.firestore()
-            .collection("recent")
-            .document(uid)
-            .collection("message")
-            .order(by: LocationData.timestamp)
-            .addSnapshotListener { querySnapshot, error in
-                if let error = error{
-                    print("에러메세지 : \(error.localizedDescription)")
-                }
-                querySnapshot?.documentChanges.forEach({ [weak self] (change) in
-                    let docId = change.document.documentID
-                    print("이게 뭘끼? \(docId)")
-                    if let index = self?.recent.firstIndex(where: { $0.id == docId }){
-                        self?.recent.remove(at: index)
-                    }
-                    if self!.isRemove{
-                        try? self?.recent.insert(change.document.data(as: Recent.self),at: 0)
-                    }
-                    self?.isRemove = true
-                })
-            }
+//        Firestore.firestore()
+//            .collection("recent")
+//            .document(uid)
+//            .collection("message")
+//            .order(by: LocationData.timestamp)
+//            .addSnapshotListener { querySnapshot, error in
+//                if let error = error{
+//                    print("에러메세지 : \(error.localizedDescription)")
+//                }
+//                querySnapshot?.documentChanges.forEach({ [weak self] (change) in
+//                    let docId = change.document.documentID
+//                    print("이게 뭘끼? \(docId)")
+//                    if let index = self?.recent.firstIndex(where: { $0.id == docId }){
+//                        self?.recent.remove(at: index)
+//                    }
+//                    if self!.isRemove{
+//                        try? self?.recent.insert(change.document.data(as: Recent.self),at: 0)
+//                    }
+//                    self?.isRemove = true
+//                })
+//            }
         
     }
     
